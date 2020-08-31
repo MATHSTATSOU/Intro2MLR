2ce3613a2804a8d11db7d45bff796d2d89425b84
Wayne Stewart <wayne.s.stewart@ou.edu>
2020-08-31 10:48:45 -0500

    Flexdashboard explained Categorical variables added with an example

---
+++ R/irr.R
@@ -0,0 +1,12 @@
+#' Irrigation data.
+#'
+#' A data set from MS giving opinions of farmers to different irrigation strategies
+#'
+#' @format A data set with 5 ros and 2 columns
+#' \describe{
+#'   \item{STRATEGY}{Categorical 5 levels}
+#'   \item{FREQ}{Frequency }
+#'   ...
+#' }
+#' @source \url{https://www.routledge.com/Statistics-for-Engineering-and-the-Sciences/Mendenhall-Sincich/p/book/9781498728850}
+"irr"
