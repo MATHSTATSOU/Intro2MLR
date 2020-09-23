f673790b53fefca1e8eccdec57407a97f1e7d1d9
Wayne Stewart <wayne.s.stewart@ou.edu>
2020-09-23 13:24:53 -0500

    Add a new function rcwidgets -- how to control one widget from within another. Essentially the copy of the doc for updateSliderInput

---
+++ R/rcwidgets.R
@@ -0,0 +1,34 @@
+## Only run this example in interactive R sessions
+#' How to control one widget from another
+#'
+#' @return Interactive browser with two widgets
+#' @export
+#'
+#' @examples
+rcwidgets <- function(){
+if (interactive()) {
+  shinyApp(
+    ui = fluidPage(
+      sidebarLayout(
+        sidebarPanel(
+          p("The first knot xk1, controls the second xk2"),
+          sliderInput("control", "Controller:", min=0, max=20, value=10,
+                      step=1),
+          sliderInput("receive", "Receiver:", min=0, max=20, value=10,
+                      step=1)
+        ),
+        mainPanel()
+      )
+    ),
+    server = function(input, output, session) {
+      observe({
+        val <- input$control
+        # Control the value, min, max, and step.
+
+        updateSliderInput(session, "receive", value = val,
+                          min = val, max = 20, step = 1)
+      })
+    }
+  )
+}
+}
