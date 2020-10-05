8bda2b5396899844901fc1bc4515d770e4f31b98
Wayne Stewart <wayne.s.stewart@ou.edu>
2020-10-05 14:31:03 -0500

    Update data

---
+++ R/ftc2.R
@@ -0,0 +1,17 @@
+#' @title ftc2 data set.
+#'
+#' @description  A dataset containing variables associated with CO emmission for smokers
+#'
+#' @details This is a standard data set from the course MATH 4753 taken from the data sets provided by the text book.
+#'
+#' @format A data frame with 25 rows and 4 variables:
+#' \describe{
+#'   \item{CO}{response}
+#'   \item{WEIGHT}{weight of tobacco}
+#'   \item{NICOTINE}{Nicotine in tobacco}
+#'   \item{TAR}{Tar in tobacco}
+#'   \item{SPECIES}{Fish species}
+#'    }
+#'
+#' @source \url{https://www.routledge.com/Statistics-for-Engineering-and-the-Sciences-Sixth-Edition/Mendenhall-Sincich/p/book/9781498728850}
+"ftc2"
