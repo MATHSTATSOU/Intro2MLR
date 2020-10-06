84220fdf4cc46cf21ace81d1d7022285f9ac6cb3
Wayne Stewart <wayne.s.stewart@ou.edu>
2020-10-06 15:53:52 -0500

    STATS in action added MS pages 627-632

---
+++ R/flag.R
@@ -0,0 +1,16 @@
+#' @title The FLAG data set.
+#'
+#' @description  A dataset containing variables associated with cost (y) of road construction.
+#'
+#' @details This is a standard data set from the course MATH 4753 taken from the data sets provided by the text book.
+#'
+#' @format A data frame with 235 rows and 10 variables:
+#' \describe{
+#'   \item{COST}{response}
+#'   \item{DOTEST}{see MS page 627}
+#'   \item{STATUS}{see MS page 627}
+#'
+#'    }
+#'
+#' @source \url{https://www.routledge.com/Statistics-for-Engineering-and-the-Sciences-Sixth-Edition/Mendenhall-Sincich/p/book/9781498728850}
+"flag"
