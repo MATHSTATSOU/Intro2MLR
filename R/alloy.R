7529d13bbd771132f57b1a16c970fbedc6a1ed99
Wayne Stewart <wayne.s.stewart@ou.edu>
2020-12-08 18:56:37 -0600

    Alloy data set corrected doc

--- R/alloy.R
+++ R/alloy.R
@@ -1,17 +1,14 @@
 #' @title Alloy data.
 #'
-#' @description  A dataset containing measurements on fish caught on the Tennessee River
+#' @description  This is a short data set from MS.
 #'
 #' @details This is a standard data set from the course MATH 4753 taken from the data sets provided by the text book.
 #'
-#' @format A data frame with 144 rows and 6 variables:
+#' @format A data frame with 5 rows and 2 variables:
 #' \describe{
-#'   \item{DDT}{ppm DDT in the flesh of fish}
-#'   \item{LENGTH}{Length of fish in cm}
-#'   \item{MILE}{Miles up river where fish is caught}
-#'   \item{RIVER}{River name}
-#'   \item{SPECIES}{Fish species}
-#'   \item{WEIGHT}{Weight of fish}
+#'   \item{AnnealtTime}{Time to anneal}
+#'   \item{PassPotent}{Postent}
+#'
 #' }
 #'
 #' @source \url{https://www.routledge.com/Statistics-for-Engineering-and-the-Sciences-Sixth-Edition/Mendenhall-Sincich/p/book/9781498728850}
